39c97d1efbde346ce330ec42d293c315bac668d0
SmSergey <ssmirnov379@gmail.com>
2023-01-17 09:07:36 +0300

    created reminder entity

---
+++ src/main/java/com/smirnov/reminder/domain/reminder/Reminder.java
@@ -0,0 +1,31 @@
+package com.smirnov.reminder.domain.reminder;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table
+public class Reminder {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter()
+    private String title;
+
+    @Setter()
+    private String description;
+
+    @Setter()
+    private LocalDateTime remind;
+
+    @Setter
+    private Long userId;
+}
